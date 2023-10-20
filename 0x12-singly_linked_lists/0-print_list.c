c04770bb9f3490ec6f555d7853f1f62c1f0ad68d
JoelALukwe <alukwe7@gmail.com>
2023-10-19 17:54:22 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_list - prints all the elements of a linked list
+* @tully: pointer to the list_t list to print
+*
+* Return: the number of nodes printed
+*/
+size_t print_list(const list_t  *tully)
+{
+size_t s = 0;
+
+while (tully)
+{
+if (!tully->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", tully->str, tully->len);
+h = tully->next;
+s++;
+}
+return (s);
+}
