409d251d18990bed42be75b7733135aa6bd7b532
JoelALukwe <alukwe7@gmail.com>
2023-12-11 17:01:25 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* Function prototype for custom _puts */
+void _puts(char *str);
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/* Using the standard puts function */
+puts("\"Programming is like building a multilingual puzzle");
+
+/* Using custom _puts function */
+_puts("\"Custom _puts function is working!");
+
+return (0);
+}
+
+/* Function definition for custom _puts */
+void _puts(char *str)
+{
+/*
+* _puts - Custom function to print a string followed by a newline.
+*
+* @str: The string to be printed.
+*
+* This function prints the provided string followed by a newline character.
+*/
+printf("%s\n", str);
+}
